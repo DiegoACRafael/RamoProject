d0169eefb91e345dc4425c85fd22aaaff82227c5
Diego Augusto <diego@bitsoftware.com.br>
2025-01-29 13:08:33 -0300

    include identity

---
+++ BackEnd/src/Application/Configurations/PagedConfiguration.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Configurations
+{
+    public static class PagedConfiguration
+    {
+        public const int DefaultStatusCode = 200;
+        public const int DefaultPageNamber = 1;
+        public const int DefaultPageSize = 10;
+    }
+}
